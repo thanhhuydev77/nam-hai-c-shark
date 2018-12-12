224ca748a65fdf6c2917cc9a4951de8e342ba6a1
Huy <thanhhuydev77@gmail.com>
2018-12-12 17:23:38 +0700

    hien thi tong tien

---
+++ Quanlyquananbobittet/Quanlyquananbobittet/DAO/TableDAO.cs
@@ -0,0 +1,31 @@
+﻿using Quanlyquananbobittet.DTO;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quanlyquananbobittet.DAO
+{
+    public class TableDAO
+    {
+        private static TableDAO instance;
+        public static int btnwidth = 87;
+        public static int btnheight = 87;
+
+        public static TableDAO Instance { get { if (instance == null) instance = new TableDAO(); return TableDAO.instance; } private set => instance = value; }
+        private TableDAO() { }
+        public List<tableDTO> loadtablelist()
+        {
+            List<tableDTO> listtable = new List<tableDTO>();
+            DataTable data = Dataprovider.Instance.excutequery("select * from tablefood"); 
+            foreach(DataRow item in data.Rows)
+            {
+                tableDTO table = new tableDTO(item);
+                listtable.Add(table);
+            }
+            return listtable;
+        }
+    }
+}
