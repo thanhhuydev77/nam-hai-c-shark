4125307fbbe47758254f12b837f7ae97cc409188
Huy <thanhhuydev77@gmail.com>
2018-12-12 17:48:53 +0700

    Revert "Revert "hien thi tong tien""
    This reverts commit 5d5da937d7445f14158a3e5ce1cd15fb492b456f.

---
+++ Quanlyquananbobittet/Quanlyquananbobittet/DAO/BillDAO.cs
@@ -0,0 +1,29 @@
+﻿using Quanlyquananbobittet.DTO;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quanlyquananbobittet.DAO
+{
+    class BillDAO
+    {
+        private static BillDAO instance;
+
+        internal static BillDAO Instance { get { if (instance == null) instance = new BillDAO(); return BillDAO.instance; } private set => instance = value; }
+       private BillDAO() { }
+        public int getuncheckbillbyid(int id)
+        {
+            DataTable data = Dataprovider.Instance.excutequery("select * from bill where idtable = "+id+"and status = 0");
+
+            if(data.Rows.Count > 0)
+            {
+                BillDTO bill = new BillDTO(data.Rows[0]);
+                return bill.ID;
+            }
+            return -1;
+        }
+    }
+}
--- Quanlyquananbobittet/Quanlyquananbobittet/ftablemanager.cs
+++ Quanlyquananbobittet/Quanlyquananbobittet/ftablemanager.cs
@@ -1,10 +1,14 @@
-﻿using System;
+﻿using Quanlyquananbobittet.DAO;
+using Quanlyquananbobittet.DTO;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Globalization;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -15,8 +19,63 @@ namespace Quanlyquananbobittet
         public ftablemanager()
         {
             InitializeComponent();
+            loadtable();
+            
+            
+        }
+        #region methos
+        void loadtable()
+        {
+           List<tableDTO> listtable = TableDAO.Instance.loadtablelist();
+            foreach (tableDTO item in listtable)
+            {
+                Button btn = new Button() { Width = TableDAO.btnwidth,Height = TableDAO.btnheight };
+                btn.Click += Btn_Click;
+                btn.Tag = item;
+                btn.BackgroundImage = new Bitmap(Application.StartupPath + "\\table.png");
+                btn.BackgroundImageLayout = ImageLayout.Stretch;
+                btn.TextAlign = ContentAlignment.TopCenter;
+                btn.Text = item.Name ;
+                btn.Font = new Font(btn.Font.FontFamily,13,FontStyle.Bold);
+
+                if (item.Status == "Trống")
+                    btn.BackColor = Color.LawnGreen;
+                else
+                    btn.BackColor = Color.IndianRed;
+
+                Fpntable.Controls.Add(btn);
+            }
+        }
+        void Showbill(int idtable)
+        {
+            float totalprice = 0;
+            Lvbill.Items.Clear();
+            List<menuDTO> listbill = MenuDAO.Instance.getmenubyid(BillDAO.Instance.getuncheckbillbyid(idtable));
+                  foreach( menuDTO menu in listbill)
+            {
+                ListViewItem item = new ListViewItem(menu.Name.ToString());
+                item.SubItems.Add(menu.Price.ToString());
+                item.SubItems.Add(menu.Count.ToString());
+                item.SubItems.Add(menu.Totalprice.ToString());
+              
+               totalprice += menu.Totalprice;
+                Lvbill.Items.Add(item);
+            }
+            CultureInfo culture = new CultureInfo("vi-VN");
+            //Tbtotalprice.Text = totalprice.ToString(currency);
+            Tbtotalprice.Text = totalprice.ToString("c", culture);
+
+
+
         }
 
+        private void Btn_Click(object sender, EventArgs e)
+        {
+            int tableid = ((sender as Button).Tag as tableDTO).ID;
+            Showbill(tableid);
+        }
+        #endregion
+        #region event
         private void thôngTinCáNhânToolStripMenuItem_Click(object sender, EventArgs e)
         {
             Finfo fi = new Finfo();
@@ -27,6 +86,12 @@ namespace Quanlyquananbobittet
         {
             Fadmin FA = new Fadmin();
             FA.ShowDialog();
+
+        }
+        #endregion
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
 
         }
     }
